f7587cbce5d451a90aac0e7566d2248ae49c28b8
BeRobi <screengsm@gmail.com>
2023-04-29 10:30:54 +0200

    tagfüggvény

--- Szemely.js
+++ Szemely.js
@@ -1,14 +1,47 @@
 class Szemely {
+  #szulNev;
+  constructor(nev, szulDatum, szuloElem) {
+    //beállítja az adattagokat
+    //a konkrét osztálypéldányra mutat a this
+    // osztálypéldány=objektum
+    this.nev = nev; //publikus adattag
+    this.szulDatum = szulDatum;
+    //this.#szulNev = nev;
+    this.setSzulNev(nev);
+    //HTML oldalon akarjuk megjeleníteni
+    szuloElem.append(` <div class="szemely">
+                        <h3>${this.nev}</h3>
+                        <p>${this.kor()} éves</p>
+                        </div>
+    
+    `);
+    // kattintásra írja ki a konzolra a személy nevét
+    // készítek egy adattagot a html elemhez
+    this.szemelyElem=$(".szemely:last-child")//csak az utolsó html DOM elem lesz
+    console.log(this.szemelyElem)
+    this.szemelyElem.on("click", ()=>{
+        console.log(this.nev)
+        // function esetén this azt az elemet jelenti amit kiváltott
+        // nyíl függvény esetén az aktuális objektumot jelenti
+    })
+  }
+  /**privát adattagok elérése
+        a tagfuggvények előtt nem kell funciton**/
+  getSzulNev() {
+    return this.#szulNev;
+  }
+  setSzulNev(nev) {
+    //ellenőrzés: nagybetűvel, nem üres, legalább 3 betű stb.
+    this.#szulNev = nev;
+  }
 
-    constructor(nev, szulDatum){
-        //beállítja az adattagokat
-        //a konkrét osztálypéldányra mutat a this
-        // osztálypéldány=objektum
-        this.nev = nev;
-        this.szulDatum = szulDatum;
-    }
-
-
+  kor() {
+    const d = new Date();
+    let year = d.getFullYear();
+    let kor = year - this.szulDatum;
+    return kor;
+  }
+  //get szulNev(){return this.#szulNev}
 }
 
 export default Szemely;
--- mian.js
+++ mian.js
@@ -1,8 +1,28 @@
-import Szemely from "./Szemely.js"
+import Szemely from "./Szemely.js";
 
-$(function(){
-  console.log("Hello")  
+$(function () {
+ const szuloElem = $(".tarolo")
+
   // példányosítás
-  const szemely1=new Szemely("Béla",1965)
+  const szemely1 = new Szemely("Béla", 1965, szuloElem); //objektum
+  const szemely2 = new Szemely("Jolán", 1975, szuloElem);
+
+  console.log(szemely1);
+  console.log(szemely2);
+  console.log(szemely1.nev);
+
+  szemely1.nev = "Kálmán";
+  console.log(szemely1);
+
+  szemely1.masikNev = "Géza";
+  console.log(szemely1);
+
+  //futásidőben változtathatóak az osztálypéldányok
+  //console.log(szemely1.#szulNev) prviát adattagot kivűlről nem érünk el
+
+  console.log(szemely1.getSzulNev())
+  console.log(szemely1.kor())
+  szemely1.setSzulNev("Vili")
   console.log(szemely1)
-})
+  //console.log(szemely1.szulNev)
+});
